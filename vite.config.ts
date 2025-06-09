ca29e221852a3f94c0e1f0db2290a343b47a2d47
Thomas Nguyen <tungvunguyennguyen@gmail.com>
2025-06-08 22:31:16 -0400

    ensure firebase app hosting deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: Number(process.env.PORT) || 5173,
+    host: true, // Allows access from network, important for containers
+  },
+  preview: {
+    port: Number(process.env.PORT) || 8080,
+    host: true, // Allows access from network, important for containers
+  },
   plugins: [react()],
   resolve: {
     alias: {
